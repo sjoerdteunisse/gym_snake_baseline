8e9a9562364969a44c4cba2253399060e6cff1f5
AXR <sjoerdteunisse@gmail.com>
2020-10-29 09:40:01 +0100

    Update snake_env.py

--- gym_snake/envs/snake_env.py
+++ gym_snake/envs/snake_env.py
@@ -20,7 +20,7 @@ log.addHandler(logging.StreamHandler())
 class SnakeEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, grid_size=[15,15], unit_size=10, unit_gap=1, snake_size=3, n_snakes=1, n_foods=1, random_init=True):
+    def __init__(self, grid_size=[6,6], unit_size=10, unit_gap=1, snake_size=2, n_snakes=1, n_foods=1, random_init=True):
         self.grid_size = grid_size
         self.unit_size = unit_size
         self.unit_gap = unit_gap
